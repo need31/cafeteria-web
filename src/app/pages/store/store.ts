84e18edb10b1fb17d28eb15284c77d93e7b85b8f
need31 <neilvargas3113@gmail.com>
2026-05-30 01:04:40 -0500

    Corregir imagenes tienda

--- src/app/pages/store/store.ts
+++ src/app/pages/store/store.ts
@@ -17,7 +17,7 @@ export class Store {
       direccion: "Av. Principal 123 - Lima",
       horario: "8:00 am - 10:00 pm",
       telefono: "999 111 222",
-      imagen: "img/tienda1.jpg"
+      imagen: "https://images.unsplash.com/photo-1554118811-1e0d58224f24"
     },
 
     {
@@ -25,7 +25,7 @@ export class Store {
       direccion: "Calle Las Flores 456",
       horario: "9:00 am - 11:00 pm",
       telefono: "999 333 444",
-      imagen: "img/tienda2.jpg"
+      imagen: "https://images.unsplash.com/photo-1509042239860-f550ce710b93"
     },
 
     {
@@ -33,7 +33,7 @@ export class Store {
       direccion: "Av. Cafetal 789",
       horario: "7:00 am - 9:00 pm",
       telefono: "999 555 666",
-      imagen: "img/tienda3.jpg"
+      imagen: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085"
     }
 
   ];
