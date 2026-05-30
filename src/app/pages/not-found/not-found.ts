dafd1f8ce7933bb06b7308f82abc27939924148e
need31 <neilvargas3113@gmail.com>
2026-05-30 01:23:08 -0500

    Agregar vercel config

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './not-found.html',
-  styleUrl: './not-found.css',
+  styleUrl: './not-found.css'
 })
 export class NotFound {}
